9ed69921fa2f265ae0a19162476ed263b75af214
Sampie <59199372+fortnite-bot@users.noreply.github.com>
2024-11-26 13:07:13 +0100

    a
    a

---
+++ opdracht_2_algoritmiek_student.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Oriëntatie op AI
+
+Final assignment: algoritmiek
+
+(c) 2019 Hogeschool Utrecht,
+Tijmen Muller (tijmen.muller@hu.nl)
+
+Opdracht:
+Beantwoord onderstaande vragen en werk onderstaande functies uit.
+Voeg commentaar toe om je code toe te lichten.
+
+Je kunt je functies testen met het gegeven raamwerk door het bestand
+uit te voeren (of met behulp van `pytest`, als je weet hoe dat werkt).
+Lever je werk in op Canvas als alle tests slagen.
+
+Let op! Het is niet toegestaan om bestaande modules te importeren en te
+        gebruiken, zoals `math` en `statistics`.
+"""
+import sys
+import inspect
+
+
+# TODO: Vul hier je naam, klas en studentnummer in.
+naam = "Yoav"
+klas = "V1B"
+studentnummer = 1885887
+
+"""
+1.  Sorteeralgoritme
+
+    Hieronder staat de pseudocode van een sorteeralgoritme:
+    1. Startend vanaf het begin van een lijst, vergelijk elk element met zijn volgende buur.
+    2. Als het element groter is dan zijn volgende buur, verwissel ze van plaats.
+    3. Doorloop zo de lijst tot het eind.
+    4. Als er verwisselingen zijn geweest bij stap 2., ga naar stap 1.
+
+    1a. Handmatig toepassen
+        Gegeven is de lijst l = [ 4, 3, 1, 2 ]. Geef de waardes die deze
+        lijst aanneemt bij álle tussenstappen bij toepassing van
+        bovenstaand sorteeralgoritme.
+"""
+#       TODO: iteration 1: [ 3, 4, 1, 2 ], iteration 2: [ 3, 1, 4, 2 ], iteration 3: [ 1, 3, 4, 2 ], iteration 4: [ 1, 3, 2, 4 ], iteration 5: [ 1, 2, 3, 4 ],  
+"""
+
+    1b. Implementatie
+        Implementeer het sorteeralgoritme in Python in een functie
+        hieronder genaamd my_sort(lst).
+        Let op: je *moet* de pseudocode volgen!
+"""
+
+def my_sort(lst):
+    count = 0
+    lst_sorted = list(lst).copy()
+    while True:
+        # raise Exception("iteraties, print en veranderingen weg halen en functie call")
+        try:
+            for i in range(len(lst_sorted) + 1):
+                if lst_sorted[count + 1]:
+                    new = lst_sorted[count + 1]  
+                    old = lst_sorted[count]        
+                    if lst_sorted[count] > lst_sorted[count+1]: 
+                        new = lst_sorted[count]
+                        old = lst_sorted[count + 1]
+                        lst_sorted[count + 1]  = new
+                        lst_sorted[count] = old   
+                        count = 0
+                    else: count = count + 1
+        except:
+            break
+            
+    """
+    Sorteer gegeven lijst volgens het algoritme zoals beschreven in de pseudocode.
+
+    1. Startend vanaf het begin van een lijst, vergelijk elk element met zijn volgende buur.
+    2. Als het element groter is dan zijn volgende buur, verwissel ze van plaats.
+    3. Doorloop zo de lijst tot het eind.
+    4. Als er verwisselingen zijn geweest bij stap 2., ga naar stap 1.
+
+    Zorg dat de gegeven lijst niet verandert, maar geef een nieuwe, gesorteerde variant van de lijst terug.
+
+    Args:
+        lst (list): Een lijst met elementen van gelijk type, bijvoorbeeld gehele getallen.
+
+    Returns:
+        list: Een nieuwe, gesorteerde variant van lijst `lst`.
+    """
+    return lst_sorted
+"""
+    1c. Best en worst case
+        -   Stel je hebt een lijst met de waarden 1, 2 en 3. Bij welke
+            volgorde van de waarden in de lijst is het sorteeralgoritme
+            het snelste klaar (best-case scenario)?
+            Hoeveel vergelijkingen (zoals beschreven in stap 1. van de
+            pseudocode) zijn nodig geweest?
+"""
+#           TODO: best case: [1,2,3]:  3 iteraties, 0 veranderingen
+"""
+
+
+        -   Bij welke volgorde van de waarden in de lijst is het
+            sorteeralgoritme het minst snel klaar (worst-case scenario)?
+            Hoeveel vergelijkingen zijn nodig geweest?
+"""
+#           TODO: worst case: [3, 2, 1]: 7 iteraties, 3 veranderingen. 
+"""
+
+
+        -   Stel je hebt een lijst met de waarden 1 tot en met 4.
+            Wat is nu het best-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+            En wat is nu het worst-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+"""
+#           TODO: best case scenario: [1,2,3,4]: 4 iteraties, 0 veranderingen. 
+#               Worst case scenario: [4,3,2,1]: 14 iteraties, 6 veranderingen
+"""
+
+
+        -   (Optioneel) Stel je hebt een lijst met de waarden 1 tot en met n
+            (je weet nu dus niet precies hoeveel waarden er in de lijst
+            zitten, het zijn er 'n').
+            Wat is nu het best-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+            En wat is nu het worst-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+"""
+#           TODO: [geef hier je antwoord]
+"""
+"""
+
+"""
+2. Linear search recursive
+    Implementeer onderstaande functie om element in de gegeven lijst te zoeken door middel van recursief lineair zoeken.
+"""
+#raise Exception('Fout: linear_search_recursive([2, 1, 15, 19, 17, 11], 17) geeft False in plaats van True')
+def linear_search_recursive(lst: list, target: int):
+    # allow = False
+    # if len(lst) > 0:
+    #     if lst[0] == target:
+    #             allow = True
+    #             print(f'target found {target}')
+    #             return True         
+    #     else:
+    #         if allow == False:          
+    #             linear_search_recursive(lst[1:], target)
+    #             return False
+
+    if not lst:
+        return False 
+    
+    if lst[0] == target:
+        return True
+
+    return linear_search_recursive(lst[1:], target)
+    """
+    Zoek een element in de gegeven lijst door middel van recursief lineair zoeken.
+
+    Zorg dat de inhoud van de gegeven lijst niet verandert.
+
+    Args:
+        lst (list):     Een lijst met elementen van gelijk type, bijvoorbeeld gehele getallen.
+        target (int):   Het gezochte element.
+
+    Returns:
+        bool: Of het element in de lijst voorkomt.
+    """
+
+"""
+(Optioneel)
+3.  Sorteeralgoritme 2
+
+    Hieronder staat de pseudocode van nog een sorteeralgoritme voor een lijst `lst` van natuurlijke getallen:
+    1. Bepaal het grootste getal k in de lijst.
+    2. Maak een nieuwe lijst van lengte k+1, waarbij elk element in deze tweede lijst begint met de waarde 0.
+    3. Tel het aantal voorkomens van elk getal in de originele lijst en sla deze frequentie op in de tweede lijst.
+    4. Maak een derde, lege lijst
+    5. Voeg elke index van de tweede lijst zo vaak toe aan de derde lijst als zij geteld is in stap 3.
+    6. Retourneer de derde lijst: zij is een gesorteerde versie van de originele lijst.
+
+    Implementeer het sorteeralgoritme in Python in een functie
+    hieronder genaamd my_sort_2(lst).
+    Let op: je *moet* de pseudocode volgen!
+"""
+
+def my_sort_2(lst):
+    """
+    Sorteer gegeven lijst volgens het algoritme zoals beschreven in de pseudocode hierboven.
+
+    Zorg dat de gegeven lijst niet verandert, maar geef een nieuwe, gesorteerde variant van de lijst terug.
+
+    Args:
+        lst (list): Een lijst met elementen van gelijk type, bijvoorbeeld gehele getallen.
+
+    Returns:
+        list: Een nieuwe, gesorteerde variant van lijst `lst`.
+    """
+    sorted_lst = []  # stap 4.
+
+    return sorted_lst
+
+
+"""
+==========================[ HU TESTRAAMWERK ]================================
+Onderstaand staan de tests voor je code -- hieronder mag je niets wijzigen!
+Je kunt je code testen door deze file te runnen of met behulp van pytest.
+"""
+import random
+
+
+def __my_assert_args(function, args, expected_output, check_type=True):
+    """
+    Controleer of gegeven functie met gegeven argumenten het verwachte resultaat oplevert.
+
+    Optioneel wordt ook het return-type gecontroleerd.
+    """
+    argstr = str(args).replace(',)', ')')
+    output = function(*args)
+
+    # Controleer eerst het return-type (optioneel)
+    if check_type:
+        msg = f"Fout: {function.__name__}{argstr} geeft geen {type(expected_output)} terug als return-type"
+        assert type(output) is type(expected_output), msg
+
+    # Controleer of de functie-uitvoer overeenkomt met de gewenste uitvoer
+    msg = f"Fout: {function.__name__}{argstr} geeft {output} in plaats van {expected_output}"
+    if type(expected_output) is float:
+        # Vergelijk bij float als return-type op 7 decimalen om afrondingsfouten te omzeilen
+        assert round(output - expected_output, 7) == 0, msg
+    else:
+        assert output == expected_output, msg
+
+
+def test_id():
+    assert naam != "", "Je moet je naam nog invullen!"
+    assert studentnummer != -1, "Je moet je studentnummer nog invullen!"
+    assert klas != "", "Je moet je klas nog invullen!"
+
+
+def test_my_sort():
+    lst_test = random.choices(range(-99, 100), k=6)
+    lst_copy = lst_test.copy()
+    lst_output = my_sort(lst_test)
+
+    assert lst_copy == lst_test, "Fout: my_sort(lst) verandert de inhoud van lijst lst"
+    assert lst_output == sorted(lst_test), \
+        f"Fout: my_sort({lst_test}) geeft {lst_output} in plaats van {sorted(lst_test)}"
+
+def test_linear_search_recursive():
+    for _ in range(10):
+        lst_test = random.sample(range(20), 6)
+        target = random.randrange(20)
+        found = target in lst_test
+        lst_copy = lst_test.copy()
+
+        outcome = linear_search_recursive(lst_test, target)
+        assert lst_copy == lst_test, "Fout: linear_search_recursive(lst, target) verandert de inhoud van lijst lst"
+        assert outcome == found, \
+            f"Fout: linear_search_recursive({lst_test}, {target}) geeft {outcome} in plaats van {found}"
+
+def test_test_linear_search_recursive_recursiveness():
+    limit = sys.getrecursionlimit()
+    sys.setrecursionlimit(50)
+    try:
+        linear_search_recursive(list(range(100)), 100)
+        assert False, "Fout: linear_search_recursive werkt niet recursief"
+    except RecursionError:
+        return
+    finally:
+        sys.setrecursionlimit(limit)   
+
+def test_my_sort_2():
+    lst_test = [random.choice(range(10)) for _ in range(10)]
+    lst_copy = lst_test.copy()
+    lst_output = my_sort_2(lst_test)
+
+    assert lst_copy == lst_test, "Fout: my_sort_2(lst) verandert de inhoud van lijst lst"
+    assert lst_output == sorted(lst_test), \
+        f"Fout: my_sort_2({lst_test}) geeft {lst_output} in plaats van {sorted(lst_test)}"
+
+
+def __main():
+    """ Test alle functies. """
+    # Noodzakelijk voor gekleurde tekst binnen een Windows terminal
+    import os
+    os.system("")
+
+    try:
+        print("\x1b[32m")   # Groene tekstkleur
+        test_id()
+
+        test_my_sort()
+        print("Je functie my_sort() werkt goed!")
+
+        test_linear_search_recursive()
+        test_test_linear_search_recursive_recursiveness()
+        print("Je functie linear_search_recursive() werkt goed!")
+
+        test_my_sort_2()
+        print("Je functie test_my_sort_2() werkt goed!")
+
+        print("\nGefeliciteerd, alles lijkt te werken!")
+        print("Lever je werk nu in op Canvas...")
+
+    except AssertionError as ae:
+        print("\x1b[31m")   # Rode tekstkleur
+        if not ae:
+            print("Je code veroorzaakt onderstaande AssertionError:")
+            raise ae
+        else:
+            print(ae)
+
+    print("\x1b[0m")    # Reset tekstkleur
+
+
+if __name__ == '__main__':
+    __main()
+    
