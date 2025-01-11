0c67da6914f422bcbdb9ba20183b47cc64e3fb2d
w_kar <walid.karroumi@student.hu.nl>
2025-01-11 22:28:28 +0100

    Ik heb hem een beetje mooier gemaakt

--- GUI.py
+++ GUI.py
@@ -11,89 +11,12 @@ from datetime import datetime
 import customtkinter as ctk
 from tkinter import messagebox
 from PIL import Image, ImageTk  # For images
-from beschrijvende import beschrijvende_statistieken
-from voorspellende import voorspellende_analyse
 
 # Instellen van het thema
 ctk.set_appearance_mode("dark")  # Kies tussen "light" en "dark"
 ctk.set_default_color_theme("blue")  # Kies een kleurthema (blauw, groen, etc.)
 
-def ai(input_message):
-    url = "https://www.promptpal.net/api/chat-gpt-no-stream"
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        "model": "gpt-4o",
-        "messages": [{"role": "user", "content": "Your prompt here " + input_message}],
-        "max_completion_tokens": 128000
-    }
-    try:
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        data = response.json()
-        response_text = str(data['response']).split('"')[5:6]
-        return response_text
-    except Exception as e:
-        print("An error occurred:", str(e))
 
-with open("db.json") as f:
-    DB_CONFIG = json.load(f)
-
-retry = False
-
-def get_db_connection():
-    global retry
-    try:
-        conn = psycopg2.connect(**DB_CONFIG)
-        return conn
-    except Exception as e:
-        print(f"Fout bij het verbinden met de database: {e}")
-        if not retry:
-            retry = True
-            return get_db_connection()
-        return None
-
-def fetch_data_from_db():
-    conn = get_db_connection()
-    if conn:
-        try:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM steam_data")
-                rows = cursor.fetchall()
-                return rows
-        except Exception as e:
-            print(f"Fout bij het ophalen van data: {e}")
-            return []
-        finally:
-            conn.close()
-    else:
-        return []
-
-def readplay():
-    conn = get_db_connection()
-    if conn:
-        try:
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    'SELECT "Playtime", "Playtime_limit", "downtime", "end_downtime", "time" FROM public."User"')
-                rows = cursor.fetchall()
-                if rows:
-                    playtime, limit, begin_downtime, end_downtime, current_time = rows[0]
-                else:
-                    playtime = 0
-                    limit = 2
-                    begin_downtime = 0
-                    end_downtime = 0
-                    current_time = 0
-                return playtime, limit, begin_downtime, end_downtime, current_time
-        except Exception as e:
-            print(f"Fout bij het ophalen van data: {e}")
-            return 0, 2, 0, 0, 0
-        finally:
-            conn.close()
-    else:
-        return 0, 2, 0, 0, 0
-
-playtime, limit, begin_downtime, end_downtime, current_time = readplay()
 
 def set_playtime_limit(limit_entry):
     try:
@@ -127,33 +50,6 @@ def find_username():
         print(f"Fout bij het vinden van de gebruikersnaam: {e}")
         return None
 
-def alerts():
-    global playtime, limit, current_time
-    n = ToastNotifier()
-    if playtime > 0:
-        if playtime < int(limit) - 2:
-            n.show_toast("Speeltijd herinnering!",
-                         f"Je hebt {playtime} uur gespeeld. Je hebt 2 uur speeltijd over. Vergeet niet om water te drinken en te rekken.",
-                         duration=10)
-        elif playtime < int(limit) - 1:
-            n.show_toast("Speeltijd herinnering!",
-                         f"Je hebt {playtime} uur gespeeld. Je hebt 1 uur speeltijd over. Zorg goed voor je gezondheid.",
-                         duration=10)
-        elif playtime >= int(limit):
-            n.show_toast("Speeltijdlimiet bereikt!",
-                         f"Je hebt {playtime} uur gespeeld. Je hebt de speeltijdlimiet bereikt. Overweeg een pauze.",
-                         duration=10)
-    elif current_time >= end_downtime:
-        n.show_toast("Pauze bereikt",
-                     "Je hebt je pauze bereikt, neem een break.",
-                     duration=10)
-    else:
-        n.show_toast("Speeltijd herinnering!",
-                     f"Je hebt {playtime} uur gespeeld. Speeltijd over: {int(limit) - int(playtime)}",
-                     duration=10)
-
-    print("Speeltijd-melding uitgevoerd.")
-    return playtime, limit
 
 # Kleuren voor het thema
 BG_COLOR = "#1F1E1E"  # Zwarte achtergrond voor het hoofdframe
